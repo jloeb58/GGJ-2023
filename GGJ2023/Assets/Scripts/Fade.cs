a84f68bf7c221750537bcefd591eeba3555ec76b
seribeengton <seribeengton@gmail.com>
2023-02-02 21:44:09 -0500

    Change Fade.cs from Start to OnEnable

--- GGJ2023/Assets/Scripts/Fade.cs
+++ GGJ2023/Assets/Scripts/Fade.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class Fade : MonoBehaviour
 {
     private Color C;
-    void Start()
+    void OnEnable()
     {
         C = GetComponent<Image>().color;
         StartCoroutine(Fading());
